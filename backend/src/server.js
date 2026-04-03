3a8fbc7a36c2d674c13e180bba60ebe05d1ee63f
xyver <xyvertucio@gmail.com>
2026-04-03 18:54:30 +0800

    backend/initial route configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
+import userRoute from './routes/user.route.js';
+import adminRoute from './routes/admin.route.js';
+import songRoute from './routes/song.route.js';
+import albumRoute from './routes/auth.route.js';
+import statsRoute from './routes/stats.route.js';
 
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
+
+app.use("/api/users", userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/songs", songRoute);
+app.use("/api/albums", albumRoute);
+app.use("/api/stats", statsRoute);
 
 app.listen(PORT, () => {
-        console.log(`Server Activate on Port: ${PORT}`);
+    console.log(`Server Activate on Port: ${PORT}`);
 })
